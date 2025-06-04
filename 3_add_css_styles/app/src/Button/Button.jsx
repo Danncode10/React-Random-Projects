d185162ed9e0b41fc810ae4200b8f6c54a9e945e
Dann <Lesterdannlopez7@gmail.com>
2025-06-04 17:51:58 +0800

    Initialize React application with Vite setup, including essential files and configurations. Add main application components, styles, and assets.

---
+++ 3_add_css_styles/app/src/Button/Button.jsx
@@ -0,0 +1,13 @@
+
+import style from './Button.module.css';
+
+function Button(){
+    return (
+        <button>
+            <span class="text">Button</span>
+        </button>
+    );
+
+}
+
+export default Button;
