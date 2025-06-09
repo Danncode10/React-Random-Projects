8c894747a157ab0b0a1beecd3d486404906711c5
Dann <Lesterdannlopez7@gmail.com>
2025-06-09 15:47:19 +0800

    feat: implement MyComponent to manage name, quantity, and comment with state

--- 4_Properties/App/src/MyComponent.jsx
+++ 4_Properties/App/src/MyComponent.jsx
@@ -1,12 +1,34 @@
-
+import React, { useState } from 'react';
 
 function MyComponent() {
     
+    const [name, setName] = useState();
+    const [quantity, setQuantity] = useState(0);
+    const [comment, setComment] = useState("");
+
+    function updateName(event){
+        setName(event.target.value);
+    }
+
+    function updateQuantity(event){
+        setQuantity(event.target.value);
+    }
+
+    function updateComment(event){
+        setComment(event.target.value);
+    }
 
     return(
-        <>
-            
-        </>
+        <div>
+            <input value={name} onChange={updateName}/>
+            <p>Name: {name}</p>
+
+            <input type="number" value={quantity} onChange={updateQuantity}/>
+            <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={updateComment} placeholder="Tell me what you want" />
+            <p>Comment: {comment}</p>
+        </div>
     )
 }
 
