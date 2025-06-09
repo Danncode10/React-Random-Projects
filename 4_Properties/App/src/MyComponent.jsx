3cb5cf291db7795d4d2abc86033dab30011be1bf
Dann <Lesterdannlopez7@gmail.com>
2025-06-09 15:28:47 +0800

    feat: implement MyComponent to manage user name, age, and employment status with state

--- 4_Properties/App/src/MyComponent.jsx
+++ 4_Properties/App/src/MyComponent.jsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
+
 
 function MyComponent() {
-    const [name, setName] = useState("Default Name");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
-
-    function updateName() {
-        setName("New Name");
-    }
-
-    const incrementAge = () => {
-        setAge(prevAge => prevAge + 1);
-    }
-
-    const toggleEmployment = () => {
-        setIsEmployed(!isEmployed);
-    }
+    
 
     return(
         <>
-            <p> Name: {name}</p>
-            <button onClick={updateName}>Set Name</button>
-            <p> Age: {age}</p>
-            <button onClick={incrementAge}>Increment Age</button>
-            <p> Employment Status: {isEmployed ? "Employed" : "Unemployed"}</p>
-            <button onClick={toggleEmployment}>
-                {isEmployed ? "Unemploy" : "Employ"}
-            </button>
+            
         </>
     )
 }
