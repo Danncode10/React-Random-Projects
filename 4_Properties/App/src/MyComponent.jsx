911c3edd1e13af51be6374bf5668afc7034fecf2
Dann <Lesterdannlopez7@gmail.com>
2025-06-09 15:54:07 +0800

    feat: refactor MyComponent to manage payment method selection

--- 4_Properties/App/src/MyComponent.jsx
+++ 4_Properties/App/src/MyComponent.jsx
@@ -1,36 +1,67 @@
 import React, { useState } from 'react';
 
 function MyComponent() {
-    
-    const [name, setName] = useState();
-    const [quantity, setQuantity] = useState(0);
-    const [comment, setComment] = useState("");
 
-    function updateName(event){
-        setName(event.target.value);
-    }
+    const [payment, setPayment] = useState("");
 
-    function updateQuantity(event){
-        setQuantity(event.target.value);
-    }
-
-    function updateComment(event){
-        setComment(event.target.value);
+    function updatePayment(event) {
+        setPayment(event.target.value);
     }
 
     return(
         <div>
-            <input value={name} onChange={updateName}/>
-            <p>Name: {name}</p>
+            <select value={payment} onChange={updatePayment}>
 
-            <input type="number" value={quantity} onChange={updateQuantity}/>
-            <p>Quantity: {quantity}</p>
+                <option value="">Select Payment Method</option>
+                <option value="credit">Credit Card</option>
+                <option value="debit">Debit Card</option>
+                <option value="paypal">PayPal</option>
 
-            <textarea value={comment} onChange={updateComment} placeholder="Tell me what you want" />
-            <p>Comment: {comment}</p>
+            </select>
+
+            <p>Selected Payment Method: {payment}</p>
         </div>
-    )
+    );
 }
 
 
 export default MyComponent;
+
+// import React, { useState } from 'react';
+
+// function MyComponent() {
+    
+//     const [name, setName] = useState();
+//     const [quantity, setQuantity] = useState(0);
+//     const [comment, setComment] = useState("");
+
+//     function updateName(event){
+//         setName(event.target.value);
+//     }
+
+//     function updateQuantity(event){
+//         setQuantity(event.target.value);
+//     }
+
+//     function updateComment(event){
+//         setComment(event.target.value);
+//     }
+
+
+
+//     return(
+//         <div>
+//             <input value={name} onChange={updateName}/>
+//             <p>Name: {name}</p>
+
+//             <input type="number" value={quantity} onChange={updateQuantity}/>
+//             <p>Quantity: {quantity}</p>
+
+//             <textarea value={comment} onChange={updateComment} placeholder="Tell me what you want" />
+//             <p>Comment: {comment}</p>
+//         </div>
+//     )
+// }
+
+
+// export default MyComponent;
