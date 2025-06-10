efd9cfb85c22f7a8c9f3aec5145139f3796b96e2
Dann <Lesterdannlopez7@gmail.com>
2025-06-10 11:14:44 +0800

    fix: update FoodList component to use functional state updates and improve food removal logic

--- 4_Properties/App/src/FoodList.jsx
+++ 4_Properties/App/src/FoodList.jsx
@@ -10,11 +10,15 @@ function FoodList(){
         // Clears the input field
         document.getElementById("foodInput").value = "";
         // Adds the new food to the list
-        setFoods([...foods, newFood]);
+        setFoods(prevFoods => [...prevFoods, newFood]);
         
     }
 
-    function handleRemoveFood(){
+    function handleRemoveFood(index){
+        // Removes the food from the list
+        setFoods(prevFoods => prevFoods.filter((_, i) => i !== index));
+        // Prevents the default action of the click event
+        return false;
 
     }
 
@@ -23,13 +27,20 @@ function FoodList(){
             <h1>Food List</h1>
             <ul>
                 {foods.map((food, index) => 
-                    <li key={index}>
+                    <li key={index} onClick={() => handleRemoveFood(index)}>
                         {food}
                     </li>
                 )}
             </ul>
-            <input type="text" placeholder="Add food" id="foodInput" />
-            <button onClick={() => handleAddFood}>Add</button>
+            <input 
+                type="text" placeholder="Add food" id="foodInput" 
+                 onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                        handleAddFood();
+                    }
+                }}
+            />
+            <button onClick={handleAddFood}>Add</button>
         </div>
 
     );
