141f29d2858444fc16d53d32ca4a87491cfc93d9
Dann <Lesterdannlopez7@gmail.com>
2025-06-11 10:09:40 +0800

    feat: integrate NameContext into ComponentD for personalized greeting

--- 4_Properties/App/src/ComponentD.jsx
+++ 4_Properties/App/src/ComponentD.jsx
@@ -1,10 +1,14 @@
-
+import { NameContext } from "./ComponentA";
+import React, {useContext} from "react";
 
 function ComponentD(){
+
+    const name = useContext(NameContext);
 
     return(
         <div className="box">
             <h1>ComponentD</h1>
+            <p>Good bye {name}</p>
         </div>
     );
 }
